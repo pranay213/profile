7135bd322b6e89c45a78d673efc8591b8fff94ec
blocmatrix <blocmatrix>
2024-05-20 01:47:12 +0530

    update

---
+++ src/Schemas/Data.js
@@ -0,0 +1,118 @@
+export const Data = {
+  profile: {
+    name: "PRANAYKUMAR KODAM",
+    ocupation: "FULL STACK DEVELOPER, MERN STACK DEVELOPER",
+    location: "Hyderabad, INDIA",
+    email: "pranaykodam.213@gmail.com",
+    telephone: "(+91) 7013643290",
+    image:
+      "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYH9z3oOm7hBVeFOex8b70wZdoj4z9fpx2upfXZMyzOx0NMboZ-V19q5WG2YwOXC6xMrWcpXoPFOEdZfBCxAy_5XMNH7WLx4DM=w532-h802", // You can replace this with the path to your image
+  },
+  aboutMe: {
+    label: "Profile Summary",
+    description:
+      "I'm very much passionate about MERN stack & Full-Stack Development.",
+  },
+  skills: {
+    technicalLabel: "Technical Skills",
+    softLabel: "Key Skills",
+    technicalSkills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MySQL",
+      "PHP",
+      "JQuery",
+      "Codeigniter",
+      "React.js",
+      "Ajax",
+      "Node.js",
+      "Rest",
+      "MongoDB",
+      "Express.js",
+      "Bootstrap",
+      "Next.js",
+      "React Native",
+      "Tailwind",
+      "Native Wind",
+      "Cloudflare",
+      "Docker",
+    ],
+    softSkills: [
+      "Problem-Solving",
+      "Attention to Detail",
+      "Time Management",
+      "Adaptability",
+    ],
+  },
+  socialMedia: {
+    label: "Social links",
+    social: [
+      {
+        label: "Visit Pranay Kumar Kodam's LinkedIn profile",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/kodam-pranay-kumar-794251136/",
+        className: "bxl-linkedin-square",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Full Stack Developer / MERN Stack Developer",
+        period: "04/2022 - Present",
+        company: "BlocMatrix IT Solutions Pvt Ltd",
+        description: [
+          "Worked on various projects involving MERN stack development.",
+          "Collaborated with team members to deliver high-quality software solutions.",
+          // Add more if needed
+        ],
+      },
+      {
+        title: "Web Developer",
+        period: "04/2021 - 11/2021",
+        company: "VHONK Digital Media and Private Ltd",
+        description: [
+          "Developed and maintained web applications using modern technologies.",
+          // Add more if needed
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "B.Tech - Electrical Electronics Engineering",
+        date: "2018",
+        institution: "Nigama Engineering College, Karimnagar",
+        grade: "Grade - 67.22%",
+      },
+    ],
+    proyects: [
+      {
+        name: "TestD",
+        description: "Medical project used for COVID vaccination.",
+        company: "BlocMatrix IT Solutions Pvt Ltd",
+        period: "", // Fill in with relevant information
+      },
+      {
+        name: "Hydroponics",
+        description:
+          "IoT-based plant project controlling plant life cycles via a mobile application.",
+        company: "BlocMatrix IT Solutions Pvt Ltd",
+        period: "", // Fill in with relevant information
+      },
+      {
+        name: "Med 360",
+        description:
+          "IoT-based health checkup device measuring ECG, temperature, and SpO2.",
+        company: "BlocMatrix IT Solutions Pvt Ltd",
+        period: "", // Fill in with relevant information
+      },
+      {
+        name: "Crickskill",
+        description: "CRM-based game application developed in React.",
+        company: "BlocMatrix IT Solutions Pvt Ltd",
+        period: "", // Fill in with relevant information
+      },
+    ],
+  },
+};
